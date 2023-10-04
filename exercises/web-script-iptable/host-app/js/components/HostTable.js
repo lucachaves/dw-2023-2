12b48c6cd21c3971b220dd22912ac6f9b229fa06
Luiz <lucachaves@gmail.com>
2023-10-04 09:56:12 -0300

    remover host

--- exercises/web-script-iptable/host-app/js/components/HostTable.js
+++ exercises/web-script-iptable/host-app/js/components/HostTable.js
@@ -1,13 +1,46 @@
-function insert(host) {
-  const tbody = document.querySelector('table tbody');
+const hosts = [];
 
-  const row = `<tr>
-      <td>${host.name}</td>
-      <td>${host.address}</td>
-      <td>${host.version}</td>
-    </tr>`;
-
-  tbody.insertAdjacentHTML('beforeend', row);
+function hasHost({ address }) {
+  return hosts.some((host) => host.address === address);
 }
 
-export default { insert };
+function hostId({ address }) {
+  return address.replaceAll('.', '');
+}
+
+function insert(host) {
+  if (!hasHost(host)) {
+    hosts.push(host);
+
+    const tbody = document.querySelector('table tbody');
+
+    const row = `<tr id="host-${hostId(host)}">
+        <td>${host.name}</td>
+        <td>${host.address}</td>
+        <td>${host.version}</td>
+        <td>
+          <span class="icon-trash" onclick="handleRemoveHost('host-${hostId(
+            host
+          )}')">
+            <iconify-icon icon="ph:trash"></iconify-icon>
+          </span>
+        </td>
+      </tr>`;
+
+    tbody.insertAdjacentHTML('beforeend', row);
+  }
+}
+
+function handleRemoveHost(host) {
+  const [, id] = host.split('-');
+
+  const hostIndex = hosts.findIndex((host) => hostId(host) === id);
+
+  hosts.splice(hostIndex, 1);
+
+  const hostRow = document.querySelector(`#${host}`);
+
+  hostRow.remove();
+}
+
+export default { insert, handleRemoveHost };
--- exercises/web-script-iptable/host-app/js/main.js
+++ exercises/web-script-iptable/host-app/js/main.js
@@ -1,12 +1,15 @@
 import 'bootstrap';
+import 'iconify-icon';
 import { hosts } from './data';
 import HostTable from './components/HostTable';
 import HostForm from './components/HostForm';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '../css/main.css';
 
 for (const host of hosts) {
   HostTable.insert(host);
 }
 
 window.handleSubmit = HostForm.handleSubmit;
+window.handleRemoveHost = HostTable.handleRemoveHost;
